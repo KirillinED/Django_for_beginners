07c643db269b7a21a653a771912e7ba3624c1439
KirillinED <jenek96@gmail.com>
2020-06-11 16:27:53 +0300

    step_2

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-# pages/views.py
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse('Hello, world!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
--- pages_project/urls.py
+++ pages_project/urls.py
@@ -1,4 +1,4 @@
-"""hello_world URL Configuration
+"""pages_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
 ]
